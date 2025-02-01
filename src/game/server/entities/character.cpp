670a2ab00d7649e13674ff443f0b4a3b9ad72c63
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2025-02-01 01:28:29 +0100

    fix

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -2765,14 +2765,16 @@ void CCharacter::HandleTiles(int Index)
 				bool IsPoliceFarmActive = GameWorld()->m_PoliceFarm.IsActive();
 				if (IsPoliceFarmActive && !m_LastPoliceFarmActive)
 					GameServer()->SendBroadcast("", m_pPlayer->GetCID(), false);
-				m_LastPoliceFarmActive = IsPoliceFarmActive;
+				
 				if (!IsPoliceFarmActive && (m_LastPoliceFarmActive || Server()->Tick() % Server()->TickSpeed() == 0))
 				{
 					char aBuf[64];
 					str_format(aBuf, sizeof(aBuf), "Too many players on police tiles [%d/%d]", GameWorld()->m_PoliceFarm.m_NumPoliceTilePlayers, GameWorld()->m_PoliceFarm.m_MaxPoliceTilePlayers);
 					GameServer()->SendBroadcast(aBuf, m_pPlayer->GetCID(), false);
+					m_LastPoliceFarmActive = IsPoliceFarmActive;
 					return;
 				}
+				m_LastPoliceFarmActive = IsPoliceFarmActive;
 			}
 			else if (ExtraMoneyTile)
 			{
