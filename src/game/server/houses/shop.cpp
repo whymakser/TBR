4c574d5f1cea8503b1025c5ff1511876272de3c3
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2025-04-15 22:55:38 +0200

    Fix some localization sdtuff

--- src/game/server/houses/shop.cpp
+++ src/game/server/houses/shop.cpp
@@ -21,21 +21,21 @@ CShop::CShop(CGameContext *pGameServer, int Type) : CHouse(pGameServer, Type)
 		m_NumItemsList = NUM_ITEMS_SHOP_LIST;
 
 		bool EuroMode = GameServer()->Config()->m_SvEuroMode;
-		AddItem("Rainbow", 5, 1500, TIME_DEATH, "Rainbow will make your tee change the color very fast.");
-		AddItem("Bloody", 15, 3500, TIME_DEATH, "Bloody will give your tee a permanent kill effect.");
-		AddItem("Police", -1, 100000, TIME_FOREVER, "Police officers get help from the police bot. For more information about the specific police ranks, please say '/police'.");
-		AddItem("Spooky Ghost", 1, 1000000, TIME_FOREVER, "Using this item you can hide from other players behind bushes. If your ghost is activated you will be able to shoot plasma projectiles. How it works: '/helptoggle'");
-		AddItem("Room Key", 16, 5000, TIME_DISCONNECT, "If you have the room key you can enter the room. It's under the spawn and there is a money tile.");
-		AddItem("VIP Classic", 1, EuroMode ? 5 : 500000, TIME_30_DAYS, "VIP Classic gives you some benefits, check '/vip'.", EuroMode);
-		AddItem("VIP+", 1, EuroMode ? 10 : 1000000, TIME_20_DAYS, "VIP+ gives you even more benefits than VIP Classic, check '/vip'.", EuroMode);
-		AddItem("Spawn Shotgun", 33, 600000, TIME_FOREVER, "You will have shotgun if you respawn. For more information about spawn weapons, please type '/spawnweapons'.");
-		AddItem("Spawn Grenade", 33, 600000, TIME_FOREVER, "You will have grenade if you respawn. For more information about spawn weapons, please type '/spawnweapons'.");
-		AddItem("Spawn Rifle", 33, 600000, TIME_FOREVER, "You will have rifle if you respawn. For more information about spawn weapons, please type '/spawnweapons'.");
-		AddItem("Ninjajetpack", 21, 10000, TIME_FOREVER, "It will make your jetpack gun be a ninja. Toggle it using '/ninjajetpack'.");
-		AddItem("Taser", 30, -1, TIME_FOREVER, "Taser is a rifle that freezes a player. For more information about the taser and your taser stats, plase visit '/taser'.");
-		AddItem("Taser battery", 30, 100000, TIME_FOREVER, "Taser battery is required to use the taser. Maximum amount of ammo is 100. Plase visit '/taser'.", false, 10);
-		AddItem("Portal Rifle", EuroMode ? 1 : 45, EuroMode ? 10 : 500000, TIME_20_DAYS, "With Portal Rifle you can create two portals where your cursor is, then teleport between them.", EuroMode);
-		AddItem("Portal Blocker", 20, 10000, TIME_FOREVER, "Create portal blockers hammer and this ammo with your cursor. See '/portal' for your current amount. How it works: '/helptoggle'", false, 10);
+		AddItem("Rainbow", 5, 1500, TIME_DEATH, Localizable("Rainbow will make your tee change the color very fast."));
+		AddItem("Bloody", 15, 3500, TIME_DEATH, Localizable("Bloody will give your tee a permanent kill effect."));
+		AddItem("Police", -1, 100000, TIME_FOREVER, Localizable("Police officers get help from the police bot. For more information about the specific police ranks, please say '/police'."));
+		AddItem("Spooky Ghost", 1, 1000000, TIME_FOREVER, Localizable("Using this item you can hide from other players behind bushes. If your ghost is activated you will be able to shoot plasma projectiles. How it works: '/helptoggle'"));
+		AddItem("Room Key", 16, 5000, TIME_DISCONNECT, Localizable("If you have the room key you can enter the room. It's under the spawn and there is a money tile."));
+		AddItem("VIP Classic", 1, EuroMode ? 5 : 500000, TIME_30_DAYS, Localizable("VIP Classic gives you some benefits, check '/vip'."), EuroMode);
+		AddItem("VIP+", 1, EuroMode ? 10 : 1000000, TIME_20_DAYS, Localizable("VIP+ gives you even more benefits than VIP Classic, check '/vip'."), EuroMode);
+		AddItem("Spawn Shotgun", 33, 600000, TIME_FOREVER, Localizable("You will have shotgun if you respawn. For more information about spawn weapons, please type '/spawnweapons'."));
+		AddItem("Spawn Grenade", 33, 600000, TIME_FOREVER, Localizable("You will have grenade if you respawn. For more information about spawn weapons, please type '/spawnweapons'."));
+		AddItem("Spawn Rifle", 33, 600000, TIME_FOREVER, Localizable("You will have rifle if you respawn. For more information about spawn weapons, please type '/spawnweapons'."));
+		AddItem("Ninjajetpack", 21, 10000, TIME_FOREVER, Localizable("It will make your jetpack gun be a ninja. Toggle it using '/ninjajetpack'."));
+		AddItem("Taser", 30, -1, TIME_FOREVER, Localizable("Taser is a rifle that freezes a player. For more information about the taser and your taser stats, plase visit '/taser'."));
+		AddItem("Taser battery", 30, 100000, TIME_FOREVER, Localizable("Taser battery is required to use the taser. Maximum amount of ammo is 100. Plase visit '/taser'."), false, 10);
+		AddItem("Portal Rifle", EuroMode ? 1 : 45, EuroMode ? 10 : 500000, TIME_20_DAYS, Localizable("With Portal Rifle you can create two portals where your cursor is, then teleport between them."), EuroMode);
+		AddItem("Portal Blocker", 20, 10000, TIME_FOREVER, Localizable("Create portal blockers hammer and this ammo with your cursor. See '/portal' for your current amount. How it works: '/helptoggle'"), false, 10);
 
 		static char aaBuf[NUM_POLICE_LEVELS][32];
 		for (int i = 0; i < NUM_POLICE_LEVELS; i++)
@@ -103,18 +103,18 @@ bool CShop::PageValid(int Page)
 const char *CShop::GetWelcomeMessage(int ClientID)
 {
 	static char aBuf[128];
-	str_format(aBuf, sizeof(aBuf), "Welcome to the shop, %s! Press F4 to start shopping.", Server()->ClientName(ClientID));
+	str_format(aBuf, sizeof(aBuf), GameServer()->m_apPlayers[ClientID]->Localize("Welcome to the shop, %s! Press F4 to start shopping."), Server()->ClientName(ClientID));
 	return aBuf;
 }
 
 const char *CShop::GetConfirmMessage(int ClientID)
 {
-	return "Are you sure you want to buy this item?";
+	return GameServer()->m_apPlayers[ClientID]->Localize("Are you sure you want to buy this item?");
 }
 
 const char *CShop::GetEndMessage(int ClientID)
 {
-	return "You canceled the purchase.";
+	return GameServer()->m_apPlayers[ClientID]->Localize("You canceled the purchase.");
 }
 
 const char *CShop::GetHeadline(int Item)
@@ -144,13 +144,13 @@ const char *CShop::GetTimeMessage(int Time)
 {
 	switch (Time)
 	{
-	case TIME_DEATH: return "You own this item until you die.";
-	case TIME_DISCONNECT: return "You own this item until\nyou disconnect.";
-	case TIME_FOREVER: return "You own this item forever.";
-	case TIME_30_DAYS: return "You own this item for 30 days.";
-	case TIME_20_DAYS: return "You own this item for 20 days.";
-	case TIME_7_DAYS: return "You own this item for 7 days.";
-	case TIME_5_DAYS: return "You own this item for 5 days.";
+	case TIME_DEATH: return Localizable("You own this item until you die.");
+	case TIME_DISCONNECT: return Localizable("You own this item until\nyou disconnect.");
+	case TIME_FOREVER: return Localizable("You own this item forever.");
+	case TIME_30_DAYS: return Localizable("You own this item for 30 days.");
+	case TIME_20_DAYS: return Localizable("You own this item for 20 days.");
+	case TIME_7_DAYS: return Localizable("You own this item for 7 days.");
+	case TIME_5_DAYS: return Localizable("You own this item for 5 days.");
 	}
 	return "Unknown";
 }
@@ -183,13 +183,13 @@ void CShop::OnPageChange(int ClientID)
 	char aMsg[512];
 	if (m_aClients[ClientID].m_Page <= PAGE_MAIN)
 	{
-		str_format(aMsg, sizeof(aMsg), "Welcome to the shop!\n\nBy shooting to the right you go one site forward, and by shooting left you go one site back.");
+		str_format(aMsg, sizeof(aMsg), GameServer()->m_apPlayers[ClientID]->Localize("Welcome to the shop!\n\nBy shooting to the right you go one site forward, and by shooting left you go one site back."));
 	}
 	else
 	{
 		char aDescription[256];
 		if (IsType(HOUSE_SHOP))
-			str_copy(aDescription, m_aItems[Item].m_pDescription, sizeof(aDescription));
+			str_copy(aDescription, GameServer()->m_apPlayers[ClientID]->Localize(m_aItems[Item].m_pDescription), sizeof(aDescription));
 		else if (IsType(HOUSE_PLOT_SHOP))
 		{
 			char aOwner[32];
@@ -199,11 +199,11 @@ void CShop::OnPageChange(int ClientID)
 			if (Owned)
 			{
 				str_format(aOwner, sizeof(aOwner), "'%s'", GameServer()->m_aPlots[Item].m_aDisplayName);
-				str_format(aRented, sizeof(aRented), "Rented until: %s", GameServer()->GetDate(GameServer()->m_aPlots[Item].m_ExpireDate));
+				str_format(aRented, sizeof(aRented), "%s: %s", GameServer()->m_apPlayers[ClientID]->Localize("Rented until"), GameServer()->GetDate(GameServer()->m_aPlots[Item].m_ExpireDate));
 			}
 			else
 			{
-				str_copy(aOwner, "for sale", sizeof(aOwner));
+				str_copy(aOwner, GameServer()->m_apPlayers[ClientID]->Localize("for sale"), sizeof(aOwner));
 				str_copy(aRented, "", sizeof(aRented));
 			}
 
@@ -229,7 +229,7 @@ void CShop::OnPageChange(int ClientID)
 
 		char aDiscountMsg[64] = "\0";
 		if (m_CurrentDiscount && m_aItems[Item].m_IsEuro)
-			str_format(aDiscountMsg, sizeof(aDiscountMsg), " (-%d%% discount!)", m_CurrentDiscount);
+			str_format(aDiscountMsg, sizeof(aDiscountMsg), " (-%d%% %s!)", m_CurrentDiscount, GameServer()->m_apPlayers[ClientID]->Localize("discount"));
 
 		str_format(aMsg, sizeof(aMsg),
 			"%s\n\n"
@@ -242,7 +242,7 @@ void CShop::OnPageChange(int ClientID)
 			m_aItems[Item].m_Level,
 			aPrice,
 			m_aItems[Item].m_IsEuro ? " EUR" : "", aDiscountMsg,
-			GetTimeMessage(m_aItems[Item].m_Time),
+			GameServer()->m_apPlayers[ClientID]->Localize(GetTimeMessage(m_aItems[Item].m_Time)),
 			aAmount,
 			aDescription,
 			(m_aItems[Item].m_IsEuro && GameServer()->Config()->m_SvEuroMode) ? "\n\nHow to get EUR ingame? Contact the admin and donate to the server, it will get added to your ingame euros.\n\nCheck '/account' for your details." : ""
@@ -252,7 +252,7 @@ void CShop::OnPageChange(int ClientID)
 	int Page = m_aClients[ClientID].m_Page;
 	if (IsType(HOUSE_SHOP) && Page > ITEM_PORTAL_RIFLE && !GameServer()->Config()->m_SvPortalRifleShop)
 		Page--;
-	SendWindow(ClientID, aMsg, "If you want to buy an item, press F3", Page);
+	SendWindow(ClientID, aMsg, GameServer()->m_apPlayers[ClientID]->Localize("If you want to buy an item, press F3"), Page);
 	m_aClients[ClientID].m_LastMotd = Server()->Tick();
 }
 
