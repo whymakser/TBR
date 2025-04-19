791e5eea59f5fcea79d4373bc0d8455d69742dc5
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2025-04-20 01:26:54 +0200

    Revert "Test upgrade version bridge determination"
    This reverts commit 358d7ee898df4331ed8afeacc6a7174e63f31a0b.

--- src/engine/shared/network.cpp
+++ src/engine/shared/network.cpp
@@ -283,7 +283,9 @@ void CNetBase::SendPacket(const NETADDR *pAddr, CNetPacketConstruct *pPacket, bo
 		}
 	}
 }
-int CNetBase::UnpackPacket(NETADDR *pAddr, unsigned char *pBuffer, CNetPacketConstruct *pPacket, bool *pSevendown, int Socket, int *pSize)
+
+// TODO: rename this function
+int CNetBase::UnpackPacket(NETADDR *pAddr, unsigned char *pBuffer, CNetPacketConstruct *pPacket, bool *pSevendown, int Socket, CNetServer *pNetServer)
 {
 	if (!m_aSocket[Socket])
 		return 1;
@@ -293,16 +295,6 @@ int CNetBase::UnpackPacket(NETADDR *pAddr, unsigned char *pBuffer, CNetPacketCon
 	if(Size <= 0)
 		return 1;
 
-	int Result = UnpackPacket(pBuffer, Size, pPacket, pSevendown);
-	if(pSize)
-		*pSize = Size;
-	return Result;
-}
-
-
-// TODO: rename this function
-int CNetBase::UnpackPacket(unsigned char *pBuffer, int Size, CNetPacketConstruct *pPacket, bool *pSevendown)
-{
 	// log the data
 	if(m_DataLogRecv)
 	{
@@ -365,8 +357,8 @@ int CNetBase::UnpackPacket(unsigned char *pBuffer, int Size, CNetPacketConstruct
 	}
 	else
 	{
-		if (pPacket->m_Flags & 1)
-			*pSevendown = false;
+		if (pNetServer)
+			*pSevendown = pNetServer->GetSevendown(pAddr, pPacket, pBuffer);
 
 		if(Size - NET_PACKETHEADERSIZE > NET_MAX_PAYLOAD)
 		{
@@ -375,22 +367,31 @@ int CNetBase::UnpackPacket(unsigned char *pBuffer, int Size, CNetPacketConstruct
 			return -1;
 		}
 
-		int HeaderSize = *pSevendown ? 3 : NET_PACKETHEADERSIZE;
+		int HeaderSize;
 		if (*pSevendown)
 		{
+			HeaderSize = 3;
+			pPacket->m_Flags = pBuffer[0]>>4;
+			pPacket->m_Ack = ((pBuffer[0]&0xf)<<8) | pBuffer[1];
 			pPacket->m_Token = NET_TOKEN_NONE;
+
 			int Flags = 0;
-			if (pPacket->m_Flags&4) Flags |= NET_PACKETFLAG_CONTROL;
-			if (pPacket->m_Flags&8) Flags |= NET_PACKETFLAG_CONNLESS;
-			if (pPacket->m_Flags&16) Flags |= NET_PACKETFLAG_RESEND;
-			if (pPacket->m_Flags&32) Flags |= NET_PACKETFLAG_COMPRESSION;
+			if (pPacket->m_Flags&1) Flags |= NET_PACKETFLAG_CONTROL;
+			if (pPacket->m_Flags&2) Flags |= NET_PACKETFLAG_CONNLESS;
+			if (pPacket->m_Flags&4) Flags |= NET_PACKETFLAG_RESEND;
+			if (pPacket->m_Flags&8) Flags |= NET_PACKETFLAG_COMPRESSION;
 			pPacket->m_Flags = Flags;
 		}
-
-		pPacket->m_Ack = ((pBuffer[0]&0x3)<<8) | pBuffer[1];
+		else
+		{
+			HeaderSize = NET_PACKETHEADERSIZE;
+			pPacket->m_Ack = ((pBuffer[0]&0x3)<<8) | pBuffer[1];
 				// xxxxxxAA AAAAAAAA
-		pPacket->m_Token = (pBuffer[3] << 24) | (pBuffer[4] << 16) | (pBuffer[5] << 8) | pBuffer[6];
+
+			pPacket->m_Token = (pBuffer[3] << 24) | (pBuffer[4] << 16) | (pBuffer[5] << 8) | pBuffer[6];
 				// TTTTTTTT TTTTTTTT TTTTTTTT TTTTTTTT
+		}
+
 		pPacket->m_NumChunks = pBuffer[2];
 				// NNNNNNNN
 		pPacket->m_DataSize = Size - HeaderSize;
